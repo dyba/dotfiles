309c79adf03217b8d9fdafebf0a2ccd57a476b12
Daniel Dyba <daniel.dyba@gmail.com>
2013-11-12 20:46:06 -0800

    Added electric return in Emacs init file

--- emacs/init.el
+++ emacs/init.el
@@ -14,7 +14,6 @@
                       clojure-mode
                       rainbow-delimiters
 		      color-theme-solarized))
-
 (dolist (p my-packages)
   (unless (package-installed-p p)
     (package-refresh-contents)
@@ -145,10 +144,45 @@
 
 ;; Requiring ElDoc
 (require 'eldoc)
+
 ;; Make ElDoc aware of ParEdit's most used commands
 (eldoc-add-command
  'paredit-backward-delete
  'paredit-close-round)
+
+(defun enable-eldoc-defaults
+  (turn-on-eldoc-mode)
+  (eldoc-add-command
+   'paredit-backward-delete
+   'pradedit-close-round))
+
+;; Adding Electric Return
+(defvar electrify-return-match
+  "[\]}\)\"]"
+  "If this regexp matches the text after the cursor, do an \"electric\" return.")
+
+(defun electrify-return-if-match (arg)
+  "If the text after the cursor matches `electrify-return-match` then open and indent an empty line between the cursor and the text. Move the cursor to the new line."
+  (interactive "P")
+  (let ((case-fold-search nil))
+    (if (looking-at electrify-return-match)
+	(save-excursion (newline-and-indent)))
+    (newline arg)
+    (indent-according-to-mode)))
+
+(defun enable-lisp-defaults
+  (lambda ()
+    (paredit-mode t)
+    (turn-on-eldoc-mode)
+    (eldoc-add-command
+     'paredit-backward-delete
+     'paredit-close-round)
+    (local-set-key (kbd "RET") 'electrify-return-if-match)
+    (eldoc-add-command 'electrify-return-if-match)
+    (show-paren-mode t)))
+
+(add-hook 'emacs-lisp-mode-hook 'enable-lisp-defaults)
+(add-hook 'clojure-mode-hook 'enable-lisp-defaults)
 
 (set-frame-font "Menlo-16")
 (custom-set-variables

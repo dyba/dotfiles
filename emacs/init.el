16d58f370857813fe82f159b60d3c120e0c7fe44
Daniel Dyba <daniel.dyba@gmail.com>
2013-12-27 17:09:33 -0800

    Use newer config for tuareg mode in Emacs

--- emacs/init.el
+++ emacs/init.el
@@ -91,22 +91,11 @@
 
 ;; -- opam and utop setup --------------------------------
 ;; Setup environment variables using opam
-(defun opam-vars ()
-  (let* ((x (shell-command-to-string "opam config env"))
-	 (x (split-string x "\n"))
-	 (x (remove-if (lambda (x) (equal x "")) x))
-	 (x (mapcar (lambda (x) (split-string x ";")) x))
-	 (x (mapcar (lambda (x) (car x)) x))
-	 (x (mapcar (lambda (x) (split-string x "=")) x))
-	 )
-    x))
-(dolist (var (opam-vars))
-  (setenv (car var) (substring (cadr var) 1 -1)))
 ;; The following simpler alternative works as of opam 1.1
-;; (dolist
-;;    (var (car (read-from-string
-;; 	       (shell-command-to-string "opam config env --sexp"))))
-;;  (setenv (car var) (cadr var)))
+(dolist
+   (var (car (read-from-string
+	      (shell-command-to-string "opam config env --sexp"))))
+  (setenv (car var) (cadr var)))
 ;; Update the emacs path
 (setq exec-path (split-string (getenv "PATH") path-separator))
 ;; Update the emacs load path

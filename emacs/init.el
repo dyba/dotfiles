bf78035e9b469493a830a2e72f6a7c0b536a221c
Daniel Dyba <daniel.dyba@pnmac.com>
2015-03-20 10:01:08 -0700

    Update emacs init file
    * Load zenburn theme by default
    * Downgrade version of cider-nrepl

--- emacs/init.el
+++ emacs/init.el
@@ -360,3 +360,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Theme Configuration
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(load-theme 'zenburn)
--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,4 +2,5 @@
  {:plugins [[lein-exec "0.3.1"]
             [lein-pprint "1.1.1"]
             [codox "0.8.10"]
-            [cider/cider-nrepl "0.9.0-SNAPSHOT"]]}}
+            [lein-environ "1.0.0"]
+            [cider/cider-nrepl "0.8.2"]]}}

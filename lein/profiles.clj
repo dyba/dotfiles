81ca2569c6782758a8868dea2cc4a846273e75e6
Daniel Dyba <daniel.dyba@pnmac.com>
2016-11-08 17:15:15 -0800

    Creates repl profile for leiningen configuration

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,11 +1,12 @@
 {:user
  {:jvm-opts ["-Xmx1024m", "-XX:PermSize=512m", "-XX:MaxPermSize=1024m"]
-  :dependencies [[org.clojure/tools.nrepl "0.2.10"]]
   :signing {:gpg-key "16D15B5A"}
   :plugins [[lein-exec "0.3.1"]
             [lein-pprint "1.1.1"]
             [codox "0.8.10"]
             [lein-set-version "0.4.1"]
-            [lein-environ "1.0.0"]
-            [refactor-nrepl "1.1.0"]
-            [cider/cider-nrepl "0.9.1"]]}}
+            [lein-environ "1.0.0"]]}
+ :repl
+ {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
+  :plugins [[refactor-nrepl "2.3.0-SNAPSHOT"]
+            [cider/cider-nrepl "0.14.0"]]}}

d8e3a1ce31ce86ef5c8d36c2bac1581b551df6c5
Daniel Dyba <daniel.dyba@gmail.com>
2017-10-10 10:37:41 -0700

    Updates dependencies in lein profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -8,6 +8,7 @@
             [lein-environ "1.0.0"]
             [lein-midje "3.2.1"]]}
  :repl
- {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
+ {:dependencies [[org.clojure/tools.nrepl "0.2.12"]
+                 [alembic "0.3.2"]]
   :plugins [[refactor-nrepl "2.3.0-SNAPSHOT"]
             [cider/cider-nrepl "0.14.0"]]}}

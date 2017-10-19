f5c9aa6d0e4ac1b08e7df11f4536f0232386ce58
Daniel Dyba <daniel.dyba@pnmac.com>
2017-10-19 10:07:22 -0700

    Adds groovy-mode to spacemacs configuration

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -65,7 +65,10 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(solidity-mode)
+   dotspacemacs-additional-packages '(
+                                      solidity-mode
+                                      groovy-mode
+                                      )
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.

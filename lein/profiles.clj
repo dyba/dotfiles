6553a4ea47a3cae9a2c96cd8f858929ae9b8c627
Daniel Dyba <daniel.dyba@pnmac.com>
2017-10-19 10:05:40 -0700

    Adds lein-cljfmt plugin to lein profiles.clj

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -5,6 +5,7 @@
             [codox "0.8.10"]
             [lein-set-version "0.4.1"]
             [lein-environ "1.0.0"]
+            [lein-cljfmt "0.5.7"]
             [lein-midje "3.2.1"]]}
  :repl
  {:dependencies [[org.clojure/tools.nrepl "0.2.12"]

45c62c29458b700aaeef8114ea5da7f939e7991a
Daniel Dyba <daniel.dyba@gmail.com>
2020-09-02 23:21:25 -0700

    Removes work-related zsh configurations

--- zsh/work-aliases.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-alias rbc="bundle exec rubocop --safe-auto-correct"
-alias ber="bundle exec rspec"
-alias std="open zoommtg://zoom.us/join?action=join&confno=4849941315"
-
-function copy-sha-uat () {
-    curl --silent https://www.bbva.uat$1.amount.com/heartbeat | tail -n 1 | awk 'NF{ print $NF }' | pbcopy
-}

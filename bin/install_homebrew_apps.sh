412d14bc4e4effacba711893bca9a3099474a523
Daniel Dyba <daniel.dyba@gmail.com>
2021-05-11 09:36:22 -0700

    Update brew install scripts

--- bin/install_homebrew_apps.sh
+++ bin/install_homebrew_apps.sh
@@ -2,3 +2,5 @@ brew cask install alacritty
 
 brew tap homebrew/cask-fonts
 brew cask install font-fira-code
+# To help manage dotfiles
+brew install stow
--- bin/install_homebrew_tools.sh
+++ bin/install_homebrew_tools.sh
@@ -7,3 +7,16 @@ brew install yapf
 
 # For cloud infrastructure
 brew install terraform
+
+brew install fd
+brew install git-delta
+brew install fzf
+brew install git-gui
+
+# For Postgres
+brew install pg_formatter
+brew install pgcli
+
+# For cli
+brew install exa # Replacement for ls
+brew install bat # View code files using cat but with syntax highlighting

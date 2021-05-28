9d44354a18f6f43e756f1abcc45e5ba45997c83d
Daniel Dyba <daniel.dyba@gmail.com>
2021-05-27 19:26:33 -0700

    Add support to install homebrew formulas either on M1 or Intel laptops

--- bin/setup_homebrew.zsh
+++ bin/setup_homebrew.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-function install_homebrew() {
+function install_homebrew {
     if ! command -v brew &> /dev/null; then
         echo "Homebrew not found. Installing..."
         arch_name="$(uname -m)"
@@ -9,9 +9,41 @@ function install_homebrew() {
             /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
         elif [ "${arch_name}" = "arm64" ]; then
             /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+            arch -x86_64 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+            /usr/sbin/softwareupdate --install-rosetta --agree-to-license
         fi
     else
         echo "Homebrew already installed."
+    fi
+}
+
+function install_brew_formula {
+    readonly formula=${1:?"Must provide name of the formula"}
+
+    arch_name="$(uname -m)"
+
+    if [ "${arch_name}" = "x86_64" ]; then
+        arch -x86_64 /usr/local/bin/brew install $formula
+    elif [ "${arch_name}" = "arm64" ]; then
+        arch -arm64 /opt/homebrew/bin/brew install $formula
+    fi
+}
+
+function install_brew_x86_formula {
+    readonly formula=${1:?"Must provide name of the formula"}
+
+    arch -x86_64 /usr/local/bin/brew install $formula
+}
+
+function is_brew_installed {
+    readonly formula=${1:?"Must provide name of the formula"}
+
+    arch_name="$(uname -m)"
+
+    if [ "${arch_name}" = "x86_64" ]; then
+        arch -x86_64 /usr/local/bin/brew list -1 | sed -E '/^=$/,//d' | grep $formula
+    elif [ "${arch_name}" = "arm64" ]; then
+        arch -arm64 /opt/homebrew/bin/brew list -1 | sed -E '/^=$/,//d' | grep $formula
     fi
 }
 
@@ -51,16 +83,51 @@ FORMULAS=(
 
     "kitty"
 
+    "macvim"
+
     # Used with spacemacs
     "golangci-lint"
+
+    "gpg-suite"
 )
 declare -r FORMULAS
 
 for formula in $FORMULAS; do
-    if ! command -v stow &> /dev/null; then
-        echo "Installing ${formula}..."
-        brew install $formula
-    else
+    is_brew_installed $formula &> /dev/null
+    if [ $? -eq 0 ]; then
         echo "${formula} already installed."
+    else
+        echo "Installing ${formula}..."
+        install_brew_formula $formula
+    fi
+done
+
+declare -a X86_FORMULAS
+X86_FORMULAS=(
+    autoconf
+    automake
+    coreutils
+    gpg
+    jemalloc
+    libffi
+    libtool
+    libxslt
+    libyaml
+    openssl
+    readline
+    sqlite3
+    unixodbc
+    vault
+    xz
+)
+declare -r X86_FORMULAS
+
+for formula in $X86_FORMULAS; do
+    is_brew_installed $formula
+    if [ $? -eq 0 ]; then
+        echo "${formula} already installed."
+    else
+        echo "Installing ${formula}..."
+        install_brew_x86_formula $formula
     fi
 done

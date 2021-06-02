51099106e684c18cab518d33ab64830a00da975f
Daniel Dyba <daniel.dyba@gmail.com>
2021-06-01 21:12:23 -0700

    Fix indentation

--- scripts/zsh/functions.zsh
+++ scripts/zsh/functions.zsh
@@ -7,10 +7,10 @@
 function envup {
     local file=$([ -z "$1" ] && echo ".env" || echo ".env.$1")
 
-	  if [ -f $file ]; then
-	      export $(cat $file | sed 's/#.*//g' | xargs)
+    if [ -f $file ]; then
+        export $(cat $file | sed 's/#.*//g' | xargs)
     else
         echo "No $file found" 1>&2
         return 1
-	  fi
+    fi
 }

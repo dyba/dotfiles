e5beb6403eb687d0b90b57d45549f8585d686665
Daniel Dyba <daniel.dyba@gmail.com>
2021-06-01 21:08:14 -0700

    Add support for more .env files

--- scripts/zsh/functions.zsh
+++ scripts/zsh/functions.zsh
@@ -1,10 +1,16 @@
+#!/bin/zsh
+
 # Load environment variables defined inside a .env file
 # Environment variables must be defined on each line using the format:
 # MY_VAR=my-value
 # Comments are ignored
-envup() {
-	if [ -f .env ]
-	then
-	    export $(cat .env | sed 's/#.*//g' | xargs)
-	fi
+function envup {
+    local file=$([ -z "$1" ] && echo ".env" || echo ".env.$1")
+
+	  if [ -f $file ]; then
+	      export $(cat $file | sed 's/#.*//g' | xargs)
+    else
+        echo "No $file found" 1>&2
+        return 1
+	  fi
 }

319215d73500f4d55cd6c7c3624770cd912372f5
Daniel Dyba <daniel.dyba@gmail.com>
2021-10-30 14:16:25 -0700

    Fix stowing for spacemacs directory
    * Exit apply symlinks script when dotfiles folder is not present

--- bin/apply_symlinks.zsh
+++ bin/apply_symlinks.zsh
@@ -1,8 +1,9 @@
 #!/usr/bin/env zsh
 
 # Applies symlinks using stow
+DOTFILES_DIR=$HOME/.dotfiles
 
-[[ -d $HOME/.dotfiles ]] && DOTFILES_DIR=$HOME/.dotfiles
+[ ! -d $DOTFILES_DIR ] && echo 'Directory .dotfiles does not exist. Clone from https://github.com/dyba/dotfiles' && exit 1
 
 declare -a PACKAGE_DIRS
 PACKAGE_DIRS=(
@@ -11,10 +12,11 @@ PACKAGE_DIRS=(
     "zsh"
     "tmux"
     "git"
-    "spacemacs"
 )
 declare -r PACKAGE_DIRS
 
 for package in $PACKAGE_DIRS; do
     stow -v --restow --target=$HOME --dir=$DOTFILES_DIR $package
 done
+
+stow -v --restow --target=$HOME --dir=$DOTFILES_DIR --ignore='.*\.(zsh|md)' spacemacs

fc06d711366ed674cb22031cbd3bb79d98cf76d8
Daniel Dyba <daniel.dyba@gmail.com>
2023-07-07 23:21:08 -0700

    chore: reindent setup homebrew zsh script

--- bin/setup_homebrew.zsh
+++ bin/setup_homebrew.zsh
@@ -1,146 +1,157 @@
 #!/usr/bin/env zsh
 
 function install_homebrew {
-    if ! command -v brew &> /dev/null; then
-        echo "Homebrew not found. Installing..."
-        arch_name="$(uname -m)"
+  arch_name="$(uname -m)"
 
-        if [ "${arch_name}" = "x86_64" ]; then
-            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        elif [ "${arch_name}" = "arm64" ]; then
-            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-            arch -x86_64 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-            /usr/sbin/softwareupdate --install-rosetta --agree-to-license
-        fi
+  if [ "${arch_name}" = "x86_64" ]; then
+    if ! command -v /usr/local/bin/brew &> /dev/null; then
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     else
-        echo "Homebrew already installed."
+      echo "Homebrew already installed on native Intel."
     fi
+  elif [ "${arch_name}" = "arm64" ]; then
+    if ! command -v /opt/homebrew/bin/brew &> /dev/null; then
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+      /usr/sbin/softwareupdate --install-rosetta --agree-to-license
+    else
+      echo "Homebrew already installed on M1."
+    fi
+  else
+    echo "Unsupported architecture ${arch_name}"
+  fi
 }
 
 function install_brew_formula {
-    readonly formula=${1:?"Must provide name of the formula"}
+  readonly formula=${1:?"Must provide name of the formula"}
 
-    arch_name="$(uname -m)"
+  arch_name="$(uname -m)"
 
-    if [ "${arch_name}" = "x86_64" ]; then
-        arch -x86_64 /usr/local/bin/brew install $formula
-    elif [ "${arch_name}" = "arm64" ]; then
-        arch -arm64 /opt/homebrew/bin/brew install $formula
-    fi
+  if [ "${arch_name}" = "x86_64" ]; then
+    arch -x86_64 /usr/local/bin/brew install $formula
+  elif [ "${arch_name}" = "arm64" ]; then
+    arch -arm64 /opt/homebrew/bin/brew install $formula
+  fi
 }
 
 function install_brew_x86_formula {
-    readonly formula=${1:?"Must provide name of the formula"}
+  readonly formula=${1:?"Must provide name of the formula"}
 
-    arch -x86_64 /usr/local/bin/brew install $formula
+  arch -x86_64 /usr/local/bin/brew install $formula
 }
 
 function is_brew_installed {
-    readonly formula=${1:?"Must provide name of the formula"}
+  readonly formula=${1:?"Must provide name of the formula"}
 
-    arch_name="$(uname -m)"
+  arch_name="$(uname -m)"
 
-    if [ "${arch_name}" = "x86_64" ]; then
-        arch -x86_64 /usr/local/bin/brew list -1 | sed -E '/^=$/,//d' | grep $formula
-    elif [ "${arch_name}" = "arm64" ]; then
-        arch -arm64 /opt/homebrew/bin/brew list -1 | sed -E '/^=$/,//d' | grep $formula
-    fi
+  if [ "${arch_name}" = "x86_64" ]; then
+    arch -x86_64 /usr/local/bin/brew list -1 | sed -E '/^=$/,//d' | grep $formula
+  elif [ "${arch_name}" = "arm64" ]; then
+    arch -arm64 /opt/homebrew/bin/brew list -1 | sed -E '/^=$/,//d' | grep $formula
+  fi
 }
 
 function is_brew_x86_installed {
-    readonly formula=${1:?"Must provide name of the formula"}
+  readonly formula=${1:?"Must provide name of the formula"}
 
-    arch -x86_64 /usr/local/bin/brew list -1 | sed -E '/^=$/,//d' | grep $formula
+  arch -x86_64 /usr/local/bin/brew list -1 | sed -E '/^=$/,//d' | grep $formula
 }
 
 install_homebrew
 
 brew tap homebrew/cask-fonts
+brew tap d12frosted/emacs-plus
 
 declare -a FORMULAS
 FORMULAS=(
-    "alacritty"
+  "alacritty"
 
-    "awscli"
+  "awscli"
 
-    "asdf"
+  "asdf"
 
-    "font-fira-code"
+  "font-fira-code"
+  "font-source-code-pro"
 
-    # Stow helps manage dotfiles
-    "stow"
+  # Stow helps manage dotfiles
+  "stow"
 
-    # For tmux support
-    "tmux"
-    "reattach-to-user-namespace"
+  # For tmux support
+  "tmux"
+  "reattach-to-user-namespace"
 
-    # For python
-    "yapf"
+  # For python
+  "yapf"
 
-    "exa" # Replacement for ls
-    "bat" # View code files using cat, but with syntax highlighting
+  # For an up-to-date pager
+  "less"
 
-    # For cloud infrastructure
-    "terraform"
+  "exa" # Replacement for ls
+  "bat" # View code files using cat, but with syntax highlighting
 
-    "fd"
-    "git-delta"
-    "fzf"
-    "git-gui"
+  # For cloud infrastructure
+  "terraform"
 
-    "pgformatter"
-    "pgcli"
+  "fd"
+  "git-delta"
+  "fzf"
+  "git-gui"
 
-    "kitty"
+  "pgformatter"
+  "pgcli"
 
-    "macvim"
-    "macdown"
+  "kitty"
 
-    # Used with spacemacs
-    "golangci-lint"
+  "macvim"
+  "macdown"
 
-    "gpg-suite"
+  "emacs-plus"
 
-    "graphviz"
+  # Used with spacemacs
+  "golangci-lint"
+
+  "gpg-suite"
+
+  "graphviz"
 )
 declare -r FORMULAS
 
 for formula in $FORMULAS; do
-    is_brew_installed $formula &> /dev/null
-    if [ $? -eq 0 ]; then
-        echo "${formula} already installed."
-    else
-        echo "Installing ${formula}..."
-        install_brew_formula $formula
-    fi
+  is_brew_installed $formula &> /dev/null
+  if [ $? -eq 0 ]; then
+    echo "${formula} already installed."
+  else
+    echo "Installing ${formula}..."
+    install_brew_formula $formula
+  fi
 done
 
 declare -a X86_FORMULAS
 X86_FORMULAS=(
-    autoconf
-    automake
-    coreutils
-    gpg
-    jemalloc
-    libffi
-    libtool
-    libxslt
-    libyaml
-    openssl
-    readline
-    sqlite3
-    unixodbc
-    vault
-    xz
+  autoconf
+  automake
+  coreutils
+  gpg
+  jemalloc
+  libffi
+  libtool
+  libxslt
+  libyaml
+  openssl
+  readline
+  sqlite3
+  unixodbc
+  vault
+  xz
 )
 declare -r X86_FORMULAS
 
 for formula in $X86_FORMULAS; do
-    is_brew_x86_installed $formula &> /dev/null
-    if [ $? -eq 0 ]; then
-        echo "${formula} already installed."
-    else
-        echo "Installing ${formula}..."
-        install_brew_x86_formula $formula
-    fi
+  is_brew_x86_installed $formula &> /dev/null
+  if [ $? -eq 0 ]; then
+    echo "${formula} already installed."
+  else
+    echo "Installing ${formula}..."
+    install_brew_x86_formula $formula
+  fi
 done

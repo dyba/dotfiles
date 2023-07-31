13b454f42e12071575b6a326302f57dd313f7bf5
Daniel Dyba <daniel.dyba@gmail.com>
2023-07-31 11:04:05 -0700

    fix: remove tilde in fzf-tab.zsh script

--- scripts/zsh/fzf-tab.zsh
+++ scripts/zsh/fzf-tab.zsh
@@ -2,7 +2,7 @@
 
 if [ ! -d $ZSH_CUSTOM/plugins/fzf-tab ]; then
     echo "Zsh plugin fzf-tab not found. Cloning into '$ZSH_CUSTOM/plugins'"
-    git clone https://github.com/Aloxaf/fzf-tab ~$ZSH_CUSTOM/plugins/fzf-tab
+    git clone https://github.com/Aloxaf/fzf-tab $ZSH_CUSTOM/plugins/fzf-tab
 fi
 
 source $ZSH_CUSTOM/plugins/fzf-tab/fzf-tab.plugin.zsh
